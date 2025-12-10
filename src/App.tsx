c7b7de1a326f1f0551844ceb47c4db73dcb367f7
Misio620 <your-email@gmail.com>
2025-12-10 18:21:35 +0800

    Fix: Switch to HashRouter for GitHub Pages compatibility

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import { Home } from './pages/Home';
 import { QuizScreen } from './pages/QuizScreen';
 import { ResultScreen } from './pages/ResultScreen';
 
 function App() {
   return (
-    <BrowserRouter basename="/pixel-game">
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/game" element={<QuizScreen />} />
         <Route path="/result" element={<ResultScreen />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
